b987872c7e4fbc23708c801eceefa023f33ec372
mitaerika <72701220+mitaerika@users.noreply.github.com>
2023-07-03 15:33:54 +0000

    delete unnecessary copies of files

--- src/app/Shared/directives/specific-alphabet.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { SpecificAlphabetDirective } from './specific-alphabet.directive';
-
-describe('SpecificAlphabetDirective', () => {
-  it('should create an instance', () => {
-    const directive = new SpecificAlphabetDirective();
-    expect(directive).toBeTruthy();
-  });
-});
