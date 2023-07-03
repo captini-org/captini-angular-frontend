93da90b6165a09122782dbc1862535ab106bea90
daniellm19 <daniellm19@ru.is>
2023-07-03 15:00:17 +0000

    Doing another commit, something spooky going on

--- frontEndCaptini/src/app/common/global.ts
+++ frontEndCaptini/src/app/common/global.ts
@@ -1,13 +1,5 @@
-<<<<<<< HEAD
-export class Global{
-    public static apiURL: string = "http://127.0.0.1:8000/";
-      
-    public static siteTitle: string = "This is example of ItSolutionStuff.com";
-}
-=======
 export class Global{
     public static apiURL: string = "http://127.0.0.1:8000/";
 
     public static siteTitle: string = "This is example of ItSolutionStuff.com";
 }
->>>>>>> 148894e8995ece9d5b0162fcc4d28ec24d0c22d8
