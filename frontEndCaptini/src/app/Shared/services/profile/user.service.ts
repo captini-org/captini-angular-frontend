20ea3346556bf8d1625e1a422a38ee7d9b506940
Insaf Khorchani <insaf@vikonnekt.com>
2023-08-11 11:27:07 +0100

    fix bug of image update

--- frontEndCaptini/src/app/Shared/services/profile/user.service.ts
+++ frontEndCaptini/src/app/Shared/services/profile/user.service.ts
@@ -60,6 +60,7 @@ export class UserService {
     fetch(api, requestOptions)
       .then((response) => {
         console.log(response);
+        window.location.reload()
       })
       .catch((error) => {
         // Handle the error and show an error message
