b70777a87f86d321d9383696c7a82703bd413fc8
mitaerika <72701220+mitaerika@users.noreply.github.com>
2023-08-17 12:14:55 +0000

    add placeholder for progress bar

--- frontEndCaptini/src/app/modules/general/components/topics/lesson/lesson.component.ts
+++ frontEndCaptini/src/app/modules/general/components/topics/lesson/lesson.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ILesson } from 'src/app/models/ILesson';
 import { ITopics } from 'src/app/models/ITopics';
+import { AuthService } from 'src/app/Shared/services/auth.service';
+import { UserService } from 'src/app/Shared/services/profile/user.service';
 import { TopicsService } from 'src/app/Shared/services/topics/topics.service';
 
 @Component({
@@ -11,6 +13,7 @@ import { TopicsService } from 'src/app/Shared/services/topics/topics.service';
 })
 export class LessonComponent implements OnInit {
   public topic_id: number | undefined;
+
   public listtopics?: ITopics[];
   public topic_by_id!: ITopics;
   public listlesson!: ILesson[];
@@ -20,11 +23,21 @@ export class LessonComponent implements OnInit {
   constructor(
     private route: ActivatedRoute,
     private topicService: TopicsService,
-    private navigate: Router
+    private navigate: Router,
+    private API: AuthService,
+    private userService: UserService,
   ) {}
 
   ngOnInit(): void {
     this.topic_id = Number(this.route.snapshot.paramMap.get('id')!);
+    let userID = this.API.getUserId();
+    this.userService.UserDetailsCatchError(userID!).subscribe((result) => {
+      if (result != null) {
+        this.Responsedata = result;
+        console.log(this.Responsedata.completed_lessons);
+      }
+    });
+
     document.body.classList.remove('teacher-bird');
     this.searchLessons();
   }
